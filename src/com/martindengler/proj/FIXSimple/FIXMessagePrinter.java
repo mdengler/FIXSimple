1e3090b687c7b888d57a390c6c0886e16159bd87
Martin Dengler <martin@martindengler.com>
2012-03-19 03:25:58 +0800

    FIXMessagePrinter.java: pretty-up prettyString()

--- src/com/martindengler/proj/FIXSimple/FIXMessagePrinter.java
+++ src/com/martindengler/proj/FIXSimple/FIXMessagePrinter.java
@@ -5,6 +5,12 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
+import java.util.Map;
+import java.util.Set;
+
+import com.martindengler.proj.FIXSimple.spec.MsgType;
+import com.martindengler.proj.FIXSimple.spec.OrdStatus;
+import com.martindengler.proj.FIXSimple.spec.Tag;
 
 
 public abstract class FIXMessagePrinter {
@@ -25,13 +31,47 @@ public abstract class FIXMessagePrinter {
 
     public static String prettyString(FIXMessage m) {
         StringBuilder sb = new StringBuilder();
-        sb.append("--------------------------------------------------");
-        sb.append(String.format("%n"));
-        sb.append(m.toString());
-        sb.append(String.format("%n"));
-        sb.append("--------------------------------------------------");
-        sb.append(String.format("%n"));
+        String nl = String.format("%n");
+
+        sb.append("---------------------" + nl);
+
+        String msgTypeString = "Message type: ";
+        if (m.containsKey(Tag.MSGTYPE)) {
+            MsgType msgType = MsgType.fromCode(m.get(Tag.MSGTYPE));
+            msgTypeString += String.format("%s\t(%s)",
+                    msgType.name(), msgType.getCode());
+        } else {
+            msgTypeString += "INVALID: no Tag.MSGTYPE key";
+        }
+        sb.append(msgTypeString);
+
+        sb.append(nl + "TargetCompID: ");
+
+        if (!m.containsKey(Tag.TARGETCOMPID)) {
+            sb.append(" <target unset>");
+        } else {
+            sb.append(m.get(Tag.TARGETCOMPID));
+        }
+
+        sb.append(nl);
+
+        int idx = 0;
+        Set<Map.Entry<Tag, String>> entrySet = m.entrySet();
+        int size = entrySet.size();
+        for (Map.Entry<Tag, String> pair : entrySet) {
+            Tag t = pair.getKey();
+            String v = pair.getValue();
+            sb.append(String.format("    %03d - %15s = %s",
+                            t.getCode(),
+                            t.name(),
+                            v));
+            sb.append(nl);
+        }
+
+        sb.append("---------------------" + nl);
+
         return sb.toString();
+
     }
 
 
