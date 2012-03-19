697083b794434afeda8408137279e927127e1ae1
Martin Dengler <martin@martindengler.com>
2012-03-19 08:18:15 +0800

    update stress.sh

--- stress.sh
+++ stress.sh
@@ -3,8 +3,9 @@ children=$1
 
 CMD="java com.martindengler.proj.FIXSimple.Initiator"
 
+retval=0
 for i in $(seq 1 $children) ; do
-    $CMD >> stress.log 2>&1 &
+    ( $CMD >> stress.log 2>&1 || echo success ) &
 done
 
 wait
